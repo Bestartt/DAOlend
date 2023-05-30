9c227a6a96d79cd0d2d33cf80c242d707188b843
mrAdiletDev <adiletdj19@gmail.com>
2023-05-30 12:44:59 +0600

    fix import

--- takesep-defi-frontend/src/pages/profile.vue
+++ takesep-defi-frontend/src/pages/profile.vue
@@ -1,11 +1,10 @@
 <script lang="ts" setup>
-    import { requestAccount } from '~/crypto';
+
 
 </script>
 
 <template>
     <h3>Профиль</h3>
 
-    <button @click="requestAccount()" class="btn btn-dark">подключить аккаунт</button>
 
 </template>
